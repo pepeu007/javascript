13d8c02555dab03bb5704e89c60facb49cf90a98
pepeu007 <24pedrinho@gmail.com>
2025-03-05 18:21:07 -0300

    Mais projetos
    Continuando o curso...

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 4) 
+
+/*while ( c <= 4){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')  
+*/
