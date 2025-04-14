d835dad9e0732876bfbd577d6dd0326d78009572
pepeu007 <24pedrinho@gmail.com>
2025-04-14 15:54:32 -0300

    Realização da Tabuada ex017 - Curso de Java Script do Curso em Video
    tabuada simples utilizando select

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('por favor digite outro número')
+    } else {
+        let n = Number(num.value)
+        let c = 1;
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c} `
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
